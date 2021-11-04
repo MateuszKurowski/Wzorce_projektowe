d382d408e8c9a086c6e42fb8bc39a95861cafb32
KaMeRr <80587569+KaMeRr@users.noreply.github.com>
2021-11-04 13:11:23 +0100

    5 zajęcia

---
+++ 5. Dekorator/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5._Dekorator
+{
+    public interface IExercise
+    {
+        void JustDoIt();
+        void AddEquipment(string equipmentName);
+    }
+
+    public class Exercise : IExercise
+    {
+        public List<string> _requiredEquipment = new();
+
+        public List<string> RequiredEquipment { get => _requiredEquipment; }
+
+        public void AddEquipment(string equipmentName) => RequiredEquipment.Add(equipmentName);
+
+        public void JustDoIt()
+        {
+            Console.WriteLine("I need to pack:");
+            RequiredEquipment.Reverse();
+            RequiredEquipment.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine("Starting exercise");
+        }
+    }
+
+    public abstract class ExerciseEquipmentDecorator : IExercise
+    {
+        IExercise _exercise;
+
+        public ExerciseEquipmentDecorator(IExercise exercise)
+        {
+            _exercise = exercise;
+        }
+
+        public virtual void JustDoIt() => _exercise.JustDoIt();
+
+        public void AddEquipment(string equipmentName) => _exercise.AddEquipment(equipmentName);
+    }
+
+    public class RunningEquipment : ExerciseEquipmentDecorator
+    {
+        public RunningEquipment(IExercise exercise) : base(exercise)
+        {
+            exercise.AddEquipment("shoes");
+        }
+
+        public override void JustDoIt()
+        {
+            Console.WriteLine("I'm about to run");
+            base.JustDoIt();
+        }
+    }
+
+    public class SwimmingPoolEquipment : ExerciseEquipmentDecorator
+    {
+        public SwimmingPoolEquipment(IExercise exercise) : base(exercise)
+        {
+            exercise.AddEquipment("flip flops");
+        }
+
+        public override void JustDoIt()
+        {
+            Console.WriteLine("I'm about to swim");
+            base.JustDoIt();
+        }
+    }
+
+    public class GolfEquipment : ExerciseEquipmentDecorator
+    {
+        public GolfEquipment(IExercise exercise) : base(exercise)
+        {
+            exercise.AddEquipment("golf club");
+            exercise.AddEquipment("balls");
+        }
+
+        public override void JustDoIt()
+        {
+            Console.WriteLine("I'm about to play golf");
+            base.JustDoIt();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var swimming = new SwimmingPoolEquipment(new Exercise());
+            swimming.JustDoIt();
+            Console.WriteLine();
+
+            var runningAndSwimming = new RunningEquipment(new SwimmingPoolEquipment(new Exercise()));
+            runningAndSwimming.JustDoIt();
+            Console.WriteLine();
+
+            var swimmingAndPlayGolf = new SwimmingPoolEquipment(new GolfEquipment(new Exercise()));
+            swimmingAndPlayGolf.JustDoIt();
+        }
+    }
+
+}
---
+++ 5. Fasada/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace WzorzecFasada
+{
+
+    interface IUserService
+    {
+        void CreateUser(string email);
+        string UsersCount();
+        void RemoveUser(string email);
+    }
+
+    static class EmailNotification
+    {
+        public static void SendEmail(string to, string subject)
+        {
+            Console.WriteLine(subject + " " + to);
+        }
+    }
+
+    public class UserRepository
+    {
+        private readonly List<string> users = new List<string>
+        {
+            "john.doe@gmail.com", "sylvester.stallone@gmail.com"
+        };
+
+        public bool IsEmailFree(string email) => !users.Contains(email);
+
+        public void AddUser(string email)
+        {
+            users.Add(email);
+        }
+
+        public int UsersCount() => users.Count;
+
+        public void RemoveUser(string email)
+        {
+            users.Remove(email);
+            EmailNotification.SendEmail(email, "Goodbye");
+        }
+    }
+
+    static class Validators
+    {
+        public static bool IsValidEmail(string email)
+        {
+            return Regex.IsMatch(email,
+                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                    RegexOptions.IgnoreCase);
+        }
+    }
+
+    class UserService : IUserService
+    {
+        private readonly UserRepository userRepository = new UserRepository();
+        public void CreateUser(string email)
+        {
+            if (!Validators.IsValidEmail(email))
+            {
+                throw new ArgumentException("Błędny email");
+            }
+
+            if (!userRepository.IsEmailFree(email))
+                throw new Exception("Podany email jest zajęty");
+
+            userRepository.AddUser(email);
+            EmailNotification.SendEmail(email, "Welcome to our service");
+        }
+
+        public string UsersCount() => $"Aktualna liczba adresow: {userRepository.UsersCount()}";
+
+        public void RemoveUser(string email)
+        {
+            if (userRepository.IsEmailFree(email))
+                throw new Exception("Podany użytkownik nie istnieje");
+            userRepository.RemoveUser(email);
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IUserService userService = new UserService();
+            Console.WriteLine(userService.UsersCount());
+            userService.CreateUser("someemail@gmail.com");
+            Console.WriteLine(userService.UsersCount());
+            userService.RemoveUser("john.doe@gmail.com");
+            Console.WriteLine(userService.UsersCount());
+        }
+    }
+
+}
