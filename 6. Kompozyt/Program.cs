589c37c9259b8e24795e6b6cd1058343acf73ef1
KaMeRr <80587569+KaMeRr@users.noreply.github.com>
2021-11-18 13:10:08 +0100

    Zajęcia 6

---
+++ 6. Kompozyt/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public interface Kompozyt
+{
+    string Nazwa { get; set; }
+    void Renderuj();
+    void DodajElement(Kompozyt element);
+    void UsunElement(Kompozyt element);
+}
+
+public class Lisc : Kompozyt
+{
+    public string Nazwa { get; set; }
+
+    public void Renderuj()
+    {
+        Console.WriteLine(Nazwa + " renderowanie...");
+    }
+
+    public void DodajElement(Kompozyt element)
+    {
+
+    }
+
+    public void UsunElement(Kompozyt element)
+    {
+    }
+    // konstruktor
+}
+
+
+public class Wezel : Kompozyt
+{
+
+    private List<Kompozyt> Lista = new List<Kompozyt>();
+
+    public string Nazwa { get; set; }
+
+    public void Renderuj()
+    {
+        Console.WriteLine($"{Nazwa} rozpoczęcie renderowania");
+
+        foreach (var item in Lista)
+        {
+            item.Renderuj();
+        }
+
+        Console.WriteLine($"{Nazwa} zakończenie renderowania");
+    }
+
+    public void DodajElement(Kompozyt element)
+    {
+        Lista.Add(element);
+    }
+
+    public void UsunElement(Kompozyt element)
+    {
+        Lista.Remove(element);
+    }
+}
+
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Wezel korzen = new Wezel();
+        korzen.Nazwa = "Korzeń";
+
+        Lisc lisc11 = new Lisc();
+        lisc11.Nazwa = "Liść 1.1";
+        korzen.DodajElement(lisc11);
+
+        Wezel wezel2 = new Wezel();
+        wezel2.Nazwa = "Węzeł 2";
+
+        Lisc lisc21 = new Lisc();
+        lisc21.Nazwa = "Liść 2.1";
+        wezel2.DodajElement(lisc21);
+
+        Lisc lisc22 = new Lisc();
+        lisc22.Nazwa = "Liść 2.2";
+        wezel2.DodajElement(lisc22);
+
+        Lisc lisc23 = new Lisc();
+        lisc23.Nazwa = "Liść 2.3";
+        wezel2.DodajElement(lisc23);
+
+        korzen.DodajElement(wezel2);
+
+        Wezel wezel3 = new Wezel();
+        wezel3.Nazwa = "Węzeł 3";
+
+        Lisc lisc31 = new Lisc();
+        lisc31.Nazwa = "Liść 3.1";
+        wezel3.DodajElement(lisc31);
+
+        Lisc lisc32 = new Lisc();
+        lisc32.Nazwa = "Liść 3.2";
+        wezel3.DodajElement(lisc32);
+
+        Wezel wezel33 = new Wezel();
+        wezel33.Nazwa = "Węzeł 3.3";
+
+        Lisc lisc331 = new Lisc();
+        lisc331.Nazwa = "Liść 3.3.1";
+        wezel33.DodajElement(lisc331);
+        wezel3.DodajElement(wezel33);
+
+        korzen.DodajElement(wezel3);
+
+        korzen.Renderuj();
+    }
+}
---
+++ 6. Most/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.IO;
+using System.Runtime;
+using System.Text;
+
+public interface ITelewizor
+{
+	int Kanal { get; set; }
+	void Wlacz();
+	void Wylacz();
+	void ZmienKanal(int kanal);
+}
+
+public class TvLg : ITelewizor
+{
+	public TvLg()
+	{
+		this.Kanal = 1;
+	}
+
+	public int Kanal { get; set; }
+
+	public void Wlacz()
+	{
+		Console.WriteLine("Telewizor LG włączony.");
+	}
+
+	public void Wylacz()
+	{
+		Console.WriteLine("Telewizor LG wyłączony.");
+	}
+
+	public void ZmienKanal(int kanal)
+	{
+		this.Kanal = kanal;
+		Console.WriteLine($"Telewizor LG, kanał: {kanal}");
+	}
+}
+
+
+
+public class TvXiaomi : ITelewizor
+{
+
+	public TvXiaomi()
+	{
+		this.Kanal = 1;
+	}
+
+	public int Kanal { get; set; }
+
+	public void Wlacz()
+	{
+		Console.WriteLine("Telewizor Xiaomi włączony.");
+	}
+
+	public void Wylacz()
+	{
+		Console.WriteLine("Telewizor Xiaomi wyłączony.");
+	}
+
+	public void ZmienKanal(int kanal)
+	{
+		this.Kanal = kanal;
+		Console.WriteLine($"Telewizor Xiaomi, kanał: {kanal}");
+	}
+
+}
+
+public abstract class PilotAbstrakcyjny
+{
+	private readonly ITelewizor tv;
+
+	public PilotAbstrakcyjny(ITelewizor tv)
+	{
+		this.tv = tv;
+	}
+
+	public void Wlacz()
+	{
+		tv.Wlacz();
+	}
+
+	public void Wylacz()
+	{
+		tv.Wylacz();
+	}
+
+	public void ZmienKanal(int kanal)
+	{
+		tv.ZmienKanal(kanal);
+	}
+}
+
+public class PilotHarmony : PilotAbstrakcyjny
+{
+	public PilotHarmony(ITelewizor tv) : base(tv) { }
+
+	public void DoWlacz()
+	{
+		Console.WriteLine("Pilot Harmony włącza telewizor...");
+        Wlacz();
+	}
+
+	public void DoWylacz()
+	{
+		Console.WriteLine("Pilot Harmony wyłącza telewizor...");
+        Wylacz();
+    }
+
+	public void DoZmienKanal(int kanal)
+	{
+		Console.WriteLine("Pilot Harmony zmienia kanał...");
+		ZmienKanal(kanal);
+	}
+}
+
+public class PilotPhilips : PilotAbstrakcyjny
+{
+	public PilotPhilips(ITelewizor tv) : base(tv) { }
+
+	public void DoWlacz()
+	{
+		Console.WriteLine("Pilot Philips włącza telewizor...");
+        Wlacz();
+    }
+
+	public void DoWylacz()
+	{
+		Console.WriteLine("Pilot Philips wyłącza telewizor...");
+        Wylacz();
+    }
+
+	public void DoZmienKanal(int kanal)
+	{
+		Console.WriteLine("Pilot Philips zmienia kanał...");
+		ZmienKanal(kanal);
+	}
+}
+
+class MainClass
+{
+	public static void Main(string[] args)
+	{
+		Console.OutputEncoding = Encoding.UTF8;
+		ITelewizor tv = new TvLg();
+		PilotHarmony pilotHarmony = new PilotHarmony(tv);
+		PilotPhilips pilotPhilips = new PilotPhilips(tv);
+
+		pilotHarmony.DoWlacz();
+		Console.WriteLine("Kanał: " + tv.Kanal);
+		pilotPhilips.DoZmienKanal(100);
+		Console.WriteLine("Kanał: " + tv.Kanal);
+		pilotHarmony.DoWylacz();
+	}
+}
