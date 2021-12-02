8471c82ee1468592778eea2d42f41726d184b564
KaMeRr <80587569+KaMeRr@users.noreply.github.com>
2021-12-02 13:04:11 +0100

    8 zajęcia

---
+++ 8. Lancuch zobowiazan/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+public interface Lancuch
+{
+    void UstawNastepne(Lancuch c);
+    void Przetwarzaj(Powiadomienia powiadomienia);
+}
+
+public class Powiadomienia
+{
+    private readonly int _number;
+
+    public Powiadomienia(int number) => _number = number;
+
+    public int PobierzLiczbe() => _number;
+}
+
+public class BrakLancuch : Lancuch
+{
+    private Lancuch _nastepneWLancuchu;
+
+    public void UstawNastepne(Lancuch c) => _nastepneWLancuchu = c;
+
+    public void Przetwarzaj(Powiadomienia powiadomienia)
+    {
+        if (powiadomienia.PobierzLiczbe() <= 0)
+            Console.WriteLine("Brak powiadomień");
+        else
+            _nastepneWLancuchu.Przetwarzaj(powiadomienia);
+    }
+}
+
+public class MaloLancuch : Lancuch
+{
+    private Lancuch _nastepneWLancuchu;
+
+    public void UstawNastepne(Lancuch c) => _nastepneWLancuchu = c;
+
+    public void Przetwarzaj(Powiadomienia powiadomienia)
+    {
+        if (powiadomienia.PobierzLiczbe() <= 5)
+            Console.WriteLine($"Mało powiadomień: {powiadomienia.PobierzLiczbe()}");
+        else
+            _nastepneWLancuchu.Przetwarzaj(powiadomienia);
+    }
+}
+
+public class DuzoLancuch : Lancuch
+{
+    private Lancuch _nastepneWLancuchu;
+
+    public void UstawNastepne(Lancuch c) => _nastepneWLancuchu = c;
+
+    public void Przetwarzaj(Powiadomienia powiadomienia) => Console.WriteLine($"Dużo powiadomień: {powiadomienia.PobierzLiczbe()}");
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Lancuch l1 = new BrakLancuch();
+        Lancuch l2 = new MaloLancuch();
+        Lancuch l3 = new DuzoLancuch();
+        l1.UstawNastepne(l2);
+        l2.UstawNastepne(l3);
+
+        var i = 0;
+        l1.Przetwarzaj(new Powiadomienia(i));
+        i++;
+        l1.Przetwarzaj(new Powiadomienia(i));
+        i += 11;
+        l1.Przetwarzaj(new Powiadomienia(i));
+        i -= 9;
+        l1.Przetwarzaj(new Powiadomienia(i));
+        i -= 3;
+        l1.Przetwarzaj(new Powiadomienia(i));
+    }
+}
---
+++ 8. Polecenie/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+public interface IPolecenie
+{
+    void Wykonaj();
+}
+
+public class KomendaWlacz : IPolecenie
+{
+    private readonly Lampa _lampa;
+    public KomendaWlacz(Lampa lampa) => _lampa = lampa;
+    public void Wykonaj() => _lampa.Wlacz();
+}
+
+public class KomendaWylacz : IPolecenie
+{
+    private readonly Lampa _lampa;
+    public KomendaWylacz(Lampa lampa) => _lampa = lampa;
+    public void Wykonaj() => _lampa.Wylacz();
+}
+
+public class Lampa
+{
+    private bool _stan;
+
+    public void Wlacz() => _stan = true;
+
+    public void Wylacz() => _stan = false;
+
+    public bool Sprawdz() => _stan;
+}
+
+public class Pilot
+{
+    private IPolecenie _polecenie;
+
+    public void UstawPolecenie(IPolecenie polecenie) => _polecenie = polecenie;
+
+    public void WcisnijGuzik() => _polecenie.Wykonaj();
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        var lampa = new Lampa();
+
+        IPolecenie wlacz = new KomendaWlacz(lampa);
+        IPolecenie wylacz = new KomendaWylacz(lampa);
+        var pilot = new Pilot();
+
+        Console.WriteLine(lampa.Sprawdz() ? "Lampa włączona" : "Lampa wyłączona");
+
+        pilot.UstawPolecenie(wlacz);
+        pilot.WcisnijGuzik();
+        Console.WriteLine(lampa.Sprawdz() ? "Lampa włączona" : "Lampa wyłączona");
+
+        pilot.UstawPolecenie(wylacz);
+        pilot.WcisnijGuzik();
+        Console.WriteLine(lampa.Sprawdz() ? "Lampa włączona" : "Lampa wyłączona");
+    }
+}
