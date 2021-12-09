52702eaa95ed931140afffa5a4b95baf458a5e44
KaMeRr <80587569+KaMeRr@users.noreply.github.com>
2021-12-09 13:12:34 +0100

    9 zajęcia

---
+++ 9. Mediator/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface IMediator
+{
+    public void DodajUzytkownika(IUzytkownik uzytkownik);
+    public void WyslijWiadomosc(string wiadomosc, IUzytkownik nadawca);
+}
+
+public interface IUzytkownik
+{
+    public void WyslijWiadomosc(string wiadomosc);
+    public void OdbierzWiadomosc(string wiadomosc);
+}
+
+public class Mediator : IMediator
+{
+    private readonly List<IUzytkownik> _uzytkownicy;
+
+    public Mediator() => _uzytkownicy = new List<IUzytkownik>();
+
+    public void DodajUzytkownika(IUzytkownik uzytkownik) => _uzytkownicy.Add(uzytkownik);
+
+    public void WyslijWiadomosc(string wiadomosc, IUzytkownik nadawca)
+    {
+
+        foreach (var item in _uzytkownicy)
+        {
+            if (item == nadawca)
+                continue;
+           item.OdbierzWiadomosc(wiadomosc);
+        }
+    }
+}
+
+public class Dev : IUzytkownik
+{
+    public string _login;
+    public IMediator _mediator;
+
+    public Dev(IMediator Mediator, string login)
+    {
+        _login = login;
+        _mediator = Mediator;
+    }
+
+    public void WyslijWiadomosc(string wiadomosc) => _mediator.WyslijWiadomosc(wiadomosc, this);
+
+    public void OdbierzWiadomosc(string wiadomosc) => Console.WriteLine("Programista " + _login + " otrzymał wiadomość: " + wiadomosc);
+}
+
+public class Klient : IUzytkownik
+{
+    public string _login;
+    public IMediator _mediator;
+
+    public Klient(IMediator Mediator, string login)
+    {
+        _login = login;
+        _mediator = Mediator;
+    }
+
+    public void WyslijWiadomosc(string wiadomosc) => _mediator.WyslijWiadomosc(wiadomosc, this);
+
+    public void OdbierzWiadomosc(string wiadomosc) => Console.WriteLine("Użytkownik " + _login + " otrzymał wiadomość: " + wiadomosc);
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        IMediator mediator = new Mediator();
+
+        IUzytkownik ania = new Klient(mediator, "Ania");
+        IUzytkownik nakamoto = new Dev(mediator, "Nakamoto");
+        IUzytkownik geohot = new Dev(mediator, "Geohot");
+
+        mediator.DodajUzytkownika(ania);
+        mediator.DodajUzytkownika(nakamoto);
+        mediator.DodajUzytkownika(geohot);
+
+        ania.WyslijWiadomosc("Proszę natychmiast wprowadzić poprawki na produkcje.");
+        geohot.WyslijWiadomosc("Czekam az Nakamoto zaparzy kawe...");
+    }
+}
---
+++ 9. Pamiatka/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Zycie
+{
+    private string _czas;
+
+    public void Set(string czas)
+    {
+        _czas =  czas;
+        Console.WriteLine("Skok do roku: " + _czas);
+    }
+
+    public Pamiatka ZapiszPamiatke()
+    {
+        Console.WriteLine("stan zapisany");
+        return new Pamiatka(_czas);
+    }
+
+    public static void PrzywrocPamiatke(Pamiatka pamiatka) => Console.WriteLine("Przywrócono rok: " + pamiatka.PobierzCzas());
+
+    public class Pamiatka
+    {
+        private readonly string _czas;
+
+        public Pamiatka(string czas) => _czas = czas;
+
+        public string PobierzCzas() => _czas;
+    }
+}
+
+static class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.WriteLine("Powrót do przyszłosci (Back to the Future)");
+        Console.WriteLine();
+
+        var zapisaneStany = new List<Zycie.Pamiatka>();
+        var zycie = new Zycie();
+
+        zycie.Set("1985");
+        zapisaneStany.Add(zycie.ZapiszPamiatke());
+        zycie.Set("1955");
+        zapisaneStany.Add(zycie.ZapiszPamiatke());
+        zycie.Set("2015");
+        zapisaneStany.Add(zycie.ZapiszPamiatke());
+        zycie.Set("1885");
+
+        Zycie.PrzywrocPamiatke(zapisaneStany[0]);
+    }
+}
