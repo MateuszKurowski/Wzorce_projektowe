dde3a4722713ac92177540266cfead21a69b3b67
KaMeRr <80587569+KaMeRr@users.noreply.github.com>
2022-01-13 22:55:04 +0100

    End

--- Interpreter/Program.cs
+++ Interpreter/Program.cs
@@ -3,96 +3,112 @@ using System.Collections.Generic;
 
 namespace Interpreter
 {
-
-    class Context
+    internal class Context
     {
+        public Context(string input) => Input = input;
 
-        private string input;
-        private int output;
+        public string Input { get; set; }
 
-        public Context(string input)
-        {
-            this.input = input;
-        }
-
-        public string Input
-        {
-            get { return input; }
-            set { input = value; }
-        }
-
-        public int Output
-        {
-            get { return output; }
-            set { output = value; }
-        }
-
+        public int Output { get; set; }
     }
 
-
-    abstract class Phrase
+    internal abstract class Phrase
     {
-
         public void Interpreter(Context context)
         {
-
             if (context.Input.Length == 0)
                 return;
 
-            /* UZUPEŁNIĆ kilka else a może while? */
-
+            if (context.Input.StartsWith(Nine()))
+            {
+                context.Output += Multiplier() * 9;
+                context.Input = context.Input[Nine().Length..];
+            }
+            if (context.Input.StartsWith(Five()))
+            {
+                context.Output += Multiplier() * 5;
+                context.Input = context.Input[Five().Length..];
+            }
+            if (context.Input.StartsWith(Four()))
+            {
+                context.Output +=  Multiplier() * 4;
+                context.Input = context.Input[Four().Length..];
+            }
+            while (context.Input.StartsWith(One()))
+            {
+                context.Output += Multiplier() * 1;
+                context.Input = context.Input[One().Length..];
+            }
         }
 
         public abstract string One();
-        //
+        public abstract string Four();
+        public abstract string Five();
+        public abstract string Nine();
         public abstract int Multiplier();
-
     }
 
-
-    class PhraseThousands : Phrase
+    internal class PhraseOnes : Phrase
     {
-        public override string One() { return "M"; }
-        public override string Four() { return " "; }
-        public override string Five() { return " "; }
-        public override string Nine() { return " "; }
-        public override int Multiplier() { return 1000; }
+        public override string One() => "I";
+        public override string Four() => "IV";
+        public override string Five() => "V";
+        public override string Nine() => "IX";
+        public override int Multiplier() => 1;
     }
 
-
-    //
-    //
-    //
-    //
-
-
-    class Apka
+    internal class PhraseTens : Phrase
     {
-        static void Main()
+        public override string One() => "X";
+        public override string Four() => "XL";
+        public override string Five() => "L";
+        public override string Nine() => "XC";
+        public override int Multiplier() => 10;
+    }
+    internal class PhraseHundreds : Phrase
+    {
+        public override string One() => "C";
+        public override string Four() => "CD";
+        public override string Five() => "D";
+        public override string Nine() => "CM";
+        public override int Multiplier() => 100;
+    }
+    internal class PhraseThousands : Phrase
+    {
+        public override string One() => "M";
+        public override string Four() => " ";
+        public override string Five() => " ";
+        public override string Nine() => " ";
+        public override int Multiplier() => 1000;
+    }
+
+    public static class Apka
+    {
+        private static void Main()
         {
+            var tree = new List<Phrase>
+            {
+                new PhraseThousands(),
+                new PhraseHundreds(),
+                new PhraseTens(),
+                new PhraseOnes()
+            };
 
-            List<Phrase> tree = new List<Phrase>();
-            tree.Add(new PhraseThousands());
-            tree.Add(new PhraseHundreds());
-
-
-            //
-            //
-            foreach (Phrase item in tree)
+            var roman = "MDXLIII";
+            var context = new Context(roman);
+            foreach (var item in tree)
             {
                 item.Interpreter(context);
             }
             Console.WriteLine(roman + " = " + context.Output);
-            // MDXLIII = 1543
-
 
             roman = "CMXVII";
             context = new Context(roman);
-            //
-            //
-            // CMXVII = 917
-
+            foreach (var item in tree)
+            {
+                item.Interpreter(context);
+            }
+            Console.WriteLine(roman + " = " + context.Output);
         }
     }
-
 }
